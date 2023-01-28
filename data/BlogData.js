513534d52db7c72d6eb2b0ad928d6a1b4bcf8946
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-01-28 12:58:48 +0800

    added technical blog: "Mastering CSS Font Spacing: Tips and Tricks for Perfect Typography"

--- data/BlogData.js
+++ data/BlogData.js
@@ -4,7 +4,7 @@ const BlogData = [
         difficulty: 'Laymen',
         title: 'Non-technical lessons I discovered since I taught myself to program.',
         description: "As a self-taught programmer, I've learned that some of the most valuable lessons go beyond just understanding code. In my latest blog post, I share the top non-technical skills that have helped me succeed in my career as a programmer.",
-        platform: 'medium',
+        platform: 'Medium',
         date: 'December 2022',
         bloglink: "https://medium.com/@dunyan/non-technical-lessons-i-discovered-since-i-taught-myself-to-program-69bf1a91448"
     },
@@ -13,7 +13,7 @@ const BlogData = [
         difficulty: 'Laymen',
         title: 'Starting to learn how to program? Read this first.',
         description: "Are you ready to start your programming journey? Make sure you're off to a great start with our guide on what you need to know before you start coding. Click here to read now!",
-        platform: 'medium',
+        platform: 'Medium',
         date: 'December 2022',
         bloglink: "https://medium.com/@dunyan/starting-to-learn-how-to-program-read-this-first-a2c207ee46b6"
     },
@@ -22,7 +22,7 @@ const BlogData = [
         difficulty: 'Laymen',
         title: 'API explained for a 5-year-old.',
         description: "Want to know the secret behind how apps and websites work together seamlessly? It's all thanks to APIs! Learn all about this exciting technology in our simple and easy-to-understand article, perfect for kids and beginners alike.",
-        platform: 'medium',
+        platform: 'Medium',
         date: 'December 2022',
         bloglink: "https://medium.com/@dunyan/api-explained-for-a-5-year-old-612978b163f2",
     },
@@ -31,9 +31,18 @@ const BlogData = [
         difficulty: 'Laymen',
         title: 'React: The Pros and Cons of a Popular JavaScript Library',
         description: 'In this article, we’ll take a closer look at the benefits and drawbacks of using React, helping you make an informed decision on whether it’s the right choice for you.',
-        platform: 'medium',
+        platform: 'Medium',
         date: 'January 2023',
         bloglink: "https://medium.com/@dunyan/react-the-pros-and-cons-of-a-popular-javascript-library-6e1e443a3e22",
+    },
+        {
+        id: 5,
+        difficulty: 'Technical',
+        title: 'Mastering CSS Font Spacing: Tips and Tricks for Perfect Typography',
+        description: 'This blog on CSS Font Spacing will cover everything you need to know about typography and font spacing in CSS and the different ways of achieving that.',
+        platform: 'Lambdatest',
+        date: 'January 2023',
+        bloglink: "https://www.lambdatest.com/blog/css-font-spacing/",
     },
   ];
 
