99f6b018f2750abaf2f59d2ba4c4aad9acd139c5
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-04-08 22:19:18 +0800

    Update MiniProjectData.js

--- data/projects/MiniProjectData.js
+++ data/projects/MiniProjectData.js
@@ -1,5 +1,13 @@
 const MiniProjectData = [
     {
+        id: 4,
+        title: 'Movie Page',
+        description: "Movie webpage that displays the most popular movies at the moment. The website is powered by a movie database and is designed to make it easy for users to find and watch the latest movies.",
+        techStack: 'NextJS + React + Tailwind + Javascript',
+        projectLink: "https://nextmovieapp-13.vercel.app",
+        githubLink: "https://github.com/dunyanong/Next-13-Movie-App"
+    },       
+    {
         id: 3,
         title: 'Mern Stack AI Generator',
         description: "web application that uses the MERN stack and DALL E's machine learning algorithms to generate personalized and intelligent content",
@@ -20,8 +28,8 @@ const MiniProjectData = [
         title: 'Birthday List',
         description: "A simple full-stack birthday list.",
         techStack: 'React + Tailwind + NextJS + Prisma + Typescript + MySQL',
-        projectLink: "https://Tokenium.vercel.app",
-        githubLink: "https://github.com/dunyanong/Tokenium"
+        projectLink: "birthdayapp4fun.vercel.app",
+        githubLink: "https://github.com/dunyanong/birthdayapp"
     },    
   ];
 
