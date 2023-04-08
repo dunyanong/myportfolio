c118c59927abd3e8c68f162766bfd154991e148c
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-04-08 22:18:23 +0800

    Update NotableProjectData.js

--- data/projects/NotableProjectData.js
+++ data/projects/NotableProjectData.js
@@ -6,17 +6,9 @@ const NotableProjectData = [
         techStack: 'Bootstrap + Javascript + HTML',
         projectLink: "https://dunyan-dev.vercel.app",
         githubLink: "https://github.com/dunyanong/Portfolio"
-    },
+    },   
     {
         id: 2,
-        title: 'Movie Page',
-        description: "Movie webpage that displays the most popular movies at the moment. The website is powered by a movie database and is designed to make it easy for users to find and watch the latest movies.",
-        techStack: 'NextJS + React + Tailwind + Javascript',
-        projectLink: "https://nextmovieapp-13.vercel.app",
-        githubLink: "https://github.com/dunyanong/Next-13-Movie-App"
-    },    
-    {
-        id: 3,
         title: 'Confessay',
         description: "A platform created to allow people to confess ther negative emotions openly and anonymously.",
         techStack: 'NextJS + DaisyUI + ReactJS + Tailwind + Firebase',
