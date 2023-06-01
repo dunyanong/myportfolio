79bd879353831ecf31746291cb47418f4a036cf0
Dun Yan <ong.dy2004@gmail.com>
2023-06-01 20:11:02 +0800

    added hackathons

--- components/Skills2.tsx
+++ components/Skills2.tsx
@@ -20,13 +20,13 @@ const Skills2 = () => {
             <div className="shadow rounded-md py-6 px-8 flex flex-col justify-start space-y-6 h-full bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div className="flex items-center">
                     <Image src={frontend} alt='frontend image' width={50} height={50}/>
-                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700 dark:text-white leading-5">Frontend development</h3>
+                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700    leading-5">Frontend development</h3>
                 </div>
                 <div className="text-md leading-tight">
                     <p className="inline font-light text-gray-700 dark:text-gray-200">I design attractive and efficient user interfaces with HTML, CSS, and JavaScript, and have expertise in frontend frameworks, responsive design, and optimization. </p>
                 </div>
                 <div className="flex flex-wrap gap-2 justify-center">
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://nextjs.org' target='_blank'><SiNextdotjs /></a></span>
+                    <span className="text-3xl text-black    mr-1"><a href='https://nextjs.org' target='_blank'><SiNextdotjs /></a></span>
                     <span className="text-3xl text-cyan-500 mr-1"><a href='https://tailwindcss.com' target='_blank'><SiTailwindcss /></a></span>
                     <span className="text-3xl text-cyan-500 mr-1"><a href='https://reactjs.org' target='_blank'><FaReact /></a></span>
                     <span className="text-3xl text-purple-600 mr-1"><a href='https://getbootstrap.com' target='_blank'><SiBootstrap /></a></span>                    
@@ -38,13 +38,13 @@ const Skills2 = () => {
             <div className="shadow rounded-md py-6 px-8 flex flex-col justify-start space-y-6 h-full bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div className="flex items-center">
                     <Image src={backend} alt='frontend image>' width={50} height={50}/>
-                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700 dark:text-white leading-5">Backend development</h3>
+                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700    leading-5">Backend development</h3>
                 </div>
                 <div className="text-md leading-tight">
                     <p className="inline font-light text-gray-700 dark:text-gray-200">I love building APIs using a tech stack that includes Express.js, Node.js, and MongoDB. Additionally, I have experience working with Firebase on occasion.</p>
                 </div>
                 <div v-if="tools" className="flex flex-wrap gap-2 justify-center">
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://expressjs.com' target='_blank'><SiExpress /></a></span>
+                    <span className="text-3xl text-black    mr-1"><a href='https://expressjs.com' target='_blank'><SiExpress /></a></span>
                     <span className="text-3xl text-green-700 mr-1"><a href='https://nodejs.org/en/' target='_blank'><FaNodeJs /></a></span>
                     <span className="text-3xl text-green-600 mr-1"><a href='https://www.mongodb.com' target='_blank'><SiMongodb /></a></span>
                     <span className="text-3xl text-orange-500 mr-1"><a href='https://firebase.google.com' target='_blank'><SiFirebase /></a></span>
@@ -53,29 +53,29 @@ const Skills2 = () => {
             <div className="shadow rounded-md py-6 px-8 flex flex-col space-y-6 h-full bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div className="flex items-center">
                 <Image src={datascience} alt='frontend image>' width={50} height={50}/>
-                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700 dark:text-white leading-5">Data Science</h3>
+                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700    leading-5">Data Science</h3>
                 </div>
                 <div className="text-md leading-tight">
                     <p className="inline font-ligh text-gray-700 dark:text-gray-200">I have some experience in AI and Machine Learning, including developing basic algorithms and building simple models. I am proficient in Python and have some exposure to popular Data Science frameworks like TensorFlow and Pandas</p>
                 </div>
                 <div v-if="tools" className="flex flex-wrap gap-2 justify-center">
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://www.python.org' target='_blank'><FaPython /></a></span>
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://pandas.pydata.org/' target='_blank'><SiPandas /></a></span>
+                    <span className="text-3xl text-black    mr-1"><a href='https://www.python.org' target='_blank'><FaPython /></a></span>
+                    <span className="text-3xl text-black    mr-1"><a href='https://pandas.pydata.org/' target='_blank'><SiPandas /></a></span>
                     <span className="text-3xl text-orange-500 mr-1"><a href='https://www.tensorflow.org/' target='_blank'><SiTensorflow /></a></span>
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://numpy.org/' target='_blank'><SiNumpy /></a></span>                    
+                    <span className="text-3xl text-black    mr-1"><a href='https://numpy.org/' target='_blank'><SiNumpy /></a></span>                    
                 </div>
             </div>          
             <div className="shadow rounded-md py-6 px-8 flex flex-col space-y-6 h-full bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div className="flex items-center">
                 <Image src={mobiledev} alt='frontend image>' width={50} height={50}/>
-                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700 dark:text-white leading-5">Mobile App Development</h3>
+                    <h3 className="pl-2 text-lg font-semibold mb-2 text-gray-700    leading-5">Mobile App Development</h3>
                 </div>
                 <div className="text-md leading-tight">
                     <p className="inline font-ligh text-gray-700 dark:text-gray-200">I develop mobile applications using Flutter and Dart, focusing on creating responsive UIs and integrating with APIs</p>
                 </div>
                 <div v-if="tools" className="flex flex-wrap gap-2 justify-center">
                     <span className="text-3xl text-cyan-400 mr-1"><a href='https://flutter.dev' target='_blank'><SiFlutter /></a></span>
-                    <span className="text-3xl text-black dark:text-white mr-1"><a href='https://dart.dev' target='_blank'><SiDart /></a></span>                   
+                    <span className="text-3xl text-black    mr-1"><a href='https://dart.dev' target='_blank'><SiDart /></a></span>                   
                 </div>
             </div>                                                       
         </div>     
---
+++ pages/hackathons/index.tsx
@@ -0,0 +1,77 @@
+import Head from 'next/head'
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+
+import { SlCalender } from 'react-icons/sl'
+import Link from 'next/link';
+
+const Home = () => {
+    return (
+    <div>
+        <Head>
+        <title>Dun Yan</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <main className="px-10 sm:px-11 md:px-12 py-5 sm:py-6 md:py-0 max-w-3xl mx-auto w-screen">
+            <section className="min-h-screen">
+            <div>
+                <Navbar />
+            </div>            
+            <div className="flex flex-col-reverse sm:flex-row items-start">
+              <div className="flex flex-col pr-8">
+                <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white">Hackathons  🚀</h1>
+                <p className="text-gray-400 py-4">
+                As an avid participant in hackathons, I have documented my exhilarating experiences and accomplishments in various hackathons.
+                Explore my Hackathon Journey, dive into the projects I've built!
+                </p>
+              </div>
+            </div> 
+
+            {/* Hackathon info */}
+            <div className='flex justify-center pt-10'>
+
+            <ol className="relative border-l border-gray-200">                  
+                <li className="mb-10 ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white" />
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400  flex gap-2"><SlCalender/> June 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-300">Impacthack</h3>
+                    <p className="mb-4 text-base font-normal text-gray-400">Create an impact in the Malaysian SME ecosystem.</p>
+                    <Link href="/comingsoon" legacyBehavior>
+                        <a className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-300 border border-gray-200 rounded-lg">Learn more <svg className="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg></a>
+                    </Link>
+                </li>
+                <li className="mb-10 ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white" />
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 flex gap-2"><SlCalender/> May 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-300">Payhack</h3>
+                    <p className="mb-4 text-base font-normal text-gray-400">How might we directly contribute towards environmental conservation or any of the Sustainable Development Goals 2030 through the digital payments space?</p>
+                    <Link href="/hackathons/payhack2023" legacyBehavior>
+                        <a className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-300 border border-gray-200 rounded-lg">Learn more <svg className="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg></a>
+                    </Link>
+                </li>
+                <li className="ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white" />
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 flex gap-2"><SlCalender/> January 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-300">RHB Hack</h3>
+                    <p className="mb-4 text-base font-normal text-gray-400 ">How might we help the young adults in developing financial literacy to grow their wealth at an early age?</p>
+                    <Link href="/hackathons/RHB2023" legacyBehavior>
+                        <a className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-300 border border-gray-200 rounded-lg">Learn more <svg className="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg></a>
+                    </Link>
+                </li>
+            </ol>
+            </div>
+            <Footer />
+        </section>
+        </main>
+        </div>
+    );
+    
+}
+
+export default Home;
